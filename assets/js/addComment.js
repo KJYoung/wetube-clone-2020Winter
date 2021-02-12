dfbea2dfbd9e107b2395fd13face48043f74f6be
KJYoung <jykim157@naver.com>
2021-02-13 00:26:42 +0900

    Comment li Id Logic!!
    In pug, we should use '-' for using js code!!!

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,6 +1,28 @@
 import axios from "axios";
+import { doc } from "prettier";
 
 const addCommentForm = document.getElementById("jsAddComment");
+let commentList;
+let commentNumber;
+let commentNumberOuterSpan;
+let delComButton;
+
+function addFakeComment(comment) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  li.className = "jsJustWrittenComment";
+  commentList.prepend(li);
+
+  if (parseInt(commentNumber.innerHTML, 10) === 0) {
+    commentNumberOuterSpan.innerHTML = `<span id="jsCommentNumber">1 comment</span>`;
+  } else if (parseInt(commentNumber.innerHTML, 10) === 1) {
+    commentNumberOuterSpan.innerHTML = `<span id="jsCommentNumber">2 comments</span>`;
+  } else {
+    commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+  }
+}
 
 async function sendComment(comment) {
   const videoId = window.location.href.split("/videos/")[1];
@@ -11,7 +33,9 @@ async function sendComment(comment) {
       comment,
     },
   });
-  console.log(response);
+  if (response.status === 200) {
+    addFakeComment(comment);
+  }
 }
 
 function handleSubmit(event) {
@@ -22,9 +46,22 @@ function handleSubmit(event) {
   commentInput.value = "";
 }
 
+function handleDelete(event) {
+  console.log(event.target);
+  console.log(event.target.parentNode);
+}
 function init() {
+  commentList = document.getElementById("jsCommentList");
+  commentNumber = document.getElementById("jsCommentNumber");
+  commentNumberOuterSpan = document.getElementById("jsCommentNumberOuterSpan");
+  delComButton = document.getElementsByClassName("delComBtn");
+
+  for (let i = 0; i < delComButton.length; i += 1) {
+    delComButton[i].addEventListener("click", handleDelete);
+  }
   addCommentForm.addEventListener("submit", handleSubmit);
 }
+
 if (addCommentForm) {
   init();
 }
