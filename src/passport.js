804a26554b8ade7afb115bcd3229b9da723bdb83
WSL_UBUNTU_KJY <jykim157@naver.com>
2021-02-14 18:51:41 +0900

    Prestart10.3 : SocialLogin Link 수정

--- src/passport.js
+++ src/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `https://localhost:4000${routes.githubCallback}`,
+      callbackURL: `https://shrouded-scrubland-35699.herokuapp.com/${routes.githubCallback}`,
     },
     githubLoginCallback
   )
@@ -25,7 +25,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://localhost:4000${routes.facebookCallback}`,
+      callbackURL: `https://shrouded-scrubland-35699.herokuapp.com/${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"],
     },
